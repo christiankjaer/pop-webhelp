a98969b1eefb09622ebb2db81236f0698263fa8c
Christian Kjær Larsen <christianden@gmail.com>
2015-04-27 16:50:16 +0200

    Tests af question klassen

---
+++ tests/questiontests.py
@@ -0,0 +1,49 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, current_user
+from app import app, db, lm
+from app.question.models import TypeIn, Question, MultipleChoice
+from config import basedir
+import unittest
+
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_from_dict_typein(self):
+        d = {'type': 'TypeIn', 'text': 'Text1', 'answer': 'Answer1'}
+        q = Question.from_dict(d)
+        assert type(q) == TypeIn
+        assert q.text == 'Text1'
+        assert q.answer == 'Answer1'
+
+
+    def test_from_dict_mc(self):
+        d = {
+            'type': 'MultipleChoice',
+            'text': 'Text2',
+            'answer': [{
+                'text': 'Answer1',
+                'correct': True}, {
+                'text': 'Answer2',
+                'correct': False}]
+        }
+        q = Question.from_dict(d)
+        assert type(q) == MultipleChoice
+        assert q.text == 'Text2'
+        assert len(q.choices) == 2
+        assert q.choices[0].text == 'Answer1'
+        assert q.choices[0].correct
+        assert q.choices[1].text == 'Answer2'
+        assert not q.choices[1].correct
+
+if __name__ == '__main__':
+    unittest.main
