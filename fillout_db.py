8ba461ef85e71c1315d1692b083f981eaed2c469
Lukas Engedal <pqd294@alumni.ku.dk>
2015-05-08 16:05:12 +0200

    Refaktureret Question's from_dict funktion, tilføjet from_dict funktion til Subject og Threshold, tilføjet mctype til MultipleChoice

--- fillout_db.py
+++ fillout_db.py
@@ -5,7 +5,7 @@ from app.question.models import *
 data = []
 
 user = User('abc123', 'pwd', confirmed=True)
-data.append(user)
+data += [user]
 
 t1 = Threshold('TypeIn Threshold', 4)
 t2 = Threshold('Multiple Choice Threshold', 3)
@@ -25,28 +25,25 @@ tiq = {'type':'TypeIn', 'text':'What is 2 + 2?', 'subject':'TypeIn Subject',
        'answer':'4',
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
+data += [Question.from_dict(tiq)]
 
-tiq1 = Question.from_dict(tiq)
 tiq = {'type':'TypeIn', 'text':'What is 2 - 2?', 'subject':'TypeIn Subject',
        'answer':'0',
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
+data += [Question.from_dict(tiq)]
 
-tiq2 = Question.from_dict(tiq)
 tiq = {'type':'TypeIn', 'text':'What is 2 * 2?', 'subject':'TypeIn Subject',
        'answer':'4',
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
+data += [Question.from_dict(tiq)]
 
-tiq3 = Question.from_dict(tiq)
 tiq = {'type':'TypeIn', 'text':'What is 2 / 2?', 'subject':'TypeIn Subject',
        'answer':'1',
        'hints': ['Hint1', 'Hint2'],
        'weight': 2}
-
-tiq4 = Question.from_dict(tiq)
-
-data += [tiq1, tiq2, tiq3, tiq4]
+data += [Question.from_dict(tiq)]
 
 mcq = {'type':'MultipleChoice', 'text':'What is 2 + 2?',
        'subject':'Multiple Choice Subject',
@@ -55,9 +52,9 @@ mcq = {'type':'MultipleChoice', 'text':'What is 2 + 2?',
                  {'text':'5', 'correct':False},
                  {'text':'6', 'correct':False}],
        'hints': ['Hint1', 'Hint2'],
-       'weight': 1}
+       'weight': 1, 'mctype': '1'}
+data += [Question.from_dict(mcq)]
 
-mcq1 = Question.from_dict(mcq)
 mcq = {'type':'MultipleChoice', 'text':'What is 2 - 2?',
        'subject':'Multiple Choice Subject',
        'answer':[{'text':'-2', 'correct':False},
@@ -65,9 +62,9 @@ mcq = {'type':'MultipleChoice', 'text':'What is 2 - 2?',
                  {'text':'2', 'correct':False},
                  {'text':'4', 'correct':False}],
        'hints': ['Hint1', 'Hint2'],
-       'weight': 1}
+       'weight': 1, 'mctype': 'X'}
+data += [Question.from_dict(mcq)]
 
-mcq2 = Question.from_dict(mcq)
 mcq = {'type':'MultipleChoice', 'text':'What is 2 * 2?',
        'subject':'Multiple Choice Subject',
        'answer':[{'text':'2', 'correct':False},
@@ -75,10 +72,9 @@ mcq = {'type':'MultipleChoice', 'text':'What is 2 * 2?',
                  {'text':'6', 'correct':False},
                  {'text':'8', 'correct':False}],
        'hints': ['Hint1', 'Hint2'],
-       'weight': 1}
+       'weight': 1, 'mctype': 'X'}
+data += [Question.from_dict(mcq)]
 
-mcq3 = Question.from_dict(mcq)
-data += [mcq1, mcq2, mcq3]
 mq = {'type':'Matching', 'text':'Pair the items',
         'subject':'Matching Subject',
         'items': [
@@ -88,47 +84,29 @@ mq = {'type':'Matching', 'text':'Pair the items',
             ('Pineapples are', 'Spiky')],
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
-
 data += [Question.from_dict(mq)]
+
 rkq = {'type':'Ranking', 'text':'Rank these numbers',
        'subject':'Ranking Subject',
        'items':['1', '2', '3', '4'],
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
+data += [Question.from_dict(rkq)]
 
-rkq1 = Question.from_dict(rkq)
 rkq = {'type':'Ranking', 'text':'Rank these numbers',
        'subject':'Ranking Subject',
        'items':['-2', '0', '2', '4'],
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
+data += [Question.from_dict(rkq)]
 
-rkq2 = Question.from_dict(rkq)
 rkq = {'type':'Ranking', 'text':'Rank these numbers',
        'subject':'Ranking Subject',
        'items':['2', '4', '6', '8'],
        'hints': ['Hint1', 'Hint2'],
        'weight': 1}
+data += [Question.from_dict(rkq)]
 
-rkq3 = Question.from_dict(rkq)
-data += [rkq1, rkq2, rkq3]
-
-#db.session.add(user)
-#db.session.add(t1)
-#db.session.add(t4)
-#db.session.add(t3)
-#db.session.add(t2)
-#db.session.add(s1)
-#db.session.add(s2)
-#db.session.add(s3)
-#db.session.add(s4)
-#db.session.add(q1)
-#db.session.add(q2)
-#db.session.add(q3)
-#db.session.add(q4)
-#db.session.add(q5)
-#db.session.add(q6)
-#db.session.add(q7)
 for d in data:
     db.session.add(d)
 db.session.commit()
