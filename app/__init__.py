33067a7a9b531e15d0011c17aad9397a5c206e7b
Christian Kjær Larsen <christianden@gmail.com>
2015-05-13 01:10:45 +0200

    Tilføjet bedre markdown converter

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_admin import Admin
+from flask_misaka import Misaka
 
 app = Flask(__name__)
 # Load config file from 'config.py'
@@ -16,6 +17,9 @@ lm.login_view = 'login'
 adm = Admin(app)
 #adm = Admin(app, app.config['NAME'], base_template='/admin/layout.html', template_mode='bootstrap3')
 mail = Mail(app)
+
+md = Misaka(wrap=True)
+md.init_app(app)
 
 from app import views
 from app.user import views
